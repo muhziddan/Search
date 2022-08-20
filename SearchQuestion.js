ee1c05e501138e8c03119a2ab009c3e43a6aed9e
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-08-20 18:16:55 +0700

    Initial commit

---
+++ SearchQuestion.js
@@ -0,0 +1,21 @@
+/*
+
+If you know a solution is not far from the root of the tree:
+BFS
+
+If the tree is very deep and solutions are rare: 
+BFS/DFS
+
+If the tree is very wide:
+BFS
+
+If solutions are frequent but located deep in the tree:
+DFS
+
+Determining whether a path exists between two nodes:
+DFS
+
+Finding the shortest path:
+BFS
+
+*/
